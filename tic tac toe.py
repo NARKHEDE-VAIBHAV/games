fd95936014fd68b948348143f49efd399fb51d51
Vaibhav Narkhede <105349101+NARKHEDE-VAIBHAV@users.noreply.github.com>
2022-11-06 20:05:16 +0530

    Update tic tac toe.py

--- tic tac toe.py
+++ tic tac toe.py
@@ -6,24 +6,23 @@ pc= "COMPUTER"
 user = "USER"
 start = random.choice(base)
 start = int(start)
-def ender(ids):
+
+def ender(ids):  # for ending the game
     print(ids," WIN ")
     exitf = input("press enter to exit")
     exit()
 
-def playnum1(box):
+def playnum1(box): # to cheake player is win or not
     if ((("1" in playernum) and ("2" in playernum) and ("3" in playernum)) or (("4" in playernum) and ("5" in playernum) and ("6" in playernum)) or (("7" in playernum) and ("8" in playernum) and ("9" in playernum)) or (("1" in playernum) and ("4" in playernum) and ("7" in playernum)) or(("2" in playernum) and ("5" in playernum) and ("8" in playernum)) or(("9" in playernum) and ("6" in playernum) and ("3" in playernum)) or (("1" in playernum) and ("5" in playernum) and ("9" in playernum)) or (("7" in playernum) and ("5" in playernum) and ("3" in playernum))):
               print(box)
               ender(user)
-    
 
-def playpc1(box):
+def playpc1(box): # to cheke computer is win or not
     if ((("1" in compnum) and ("2" in compnum) and ("3" in compnum)) or (("4" in compnum) and ("5" in compnum) and ("6" in compnum)) or (("7" in compnum) and ("8" in compnum) and ("9" in compnum)) or (("1" in compnum) and ("4" in compnum) and ("7" in compnum)) or(("2" in compnum) and ("5" in compnum) and ("8" in compnum)) or(("9" in compnum) and ("6" in compnum) and ("3" in compnum)) or (("1" in compnum) and ("5" in compnum) and ("9" in compnum)) or (("7" in compnum) and ("5" in compnum) and ("3" in compnum))):
               print(box)
               ender(pc)  
-              
 
-def player(box):
+def player(box):  # player will start from here
   i = 1
   while (i < 11): 
     if (number == []):
@@ -31,7 +30,7 @@ def player(box):
          playpc1(box)
          print("no moves left")
          break
-    player = input(box)
+    player = input(box)  #player input
     if (player in base):
       if (player in em):
         print(box)
@@ -40,12 +39,12 @@ def player(box):
         playernum.append(player)
         em.append(player)
         number.remove(player)
-        box = box.replace(player,"X")
+        box = box.replace(player,"X") 
         playnum1(box)
         if (number == []):
           print(box)
           print("no moves left")
-        comp = random.choice(number)
+        comp = random.choice(number) # computer input
         compnum.append(comp)
         em.append(comp)
         number.remove(comp)
@@ -54,7 +53,7 @@ def player(box):
     else:
        print("error")   
 
-def comp(box):
+def comp(box):  # computer will start form here
     comp = random.choice(number)
     compnum.append(comp)
     em.append(comp)
@@ -70,7 +69,7 @@ def comp(box):
          print("no moves left")
          break
        else:
-         player = input(box)
+         player = input(box)  # player input
          if (player in base):
            if (player in em):
               print(box)
@@ -81,7 +80,7 @@ def comp(box):
               number.remove(player)
               box = box.replace(player,"X")
               playnum1(box)
-              comp = random.choice(number)
+              comp = random.choice(number)  #computer input
               compnum.append(comp)
               em.append(comp)
               number.remove(comp) 
@@ -89,7 +88,7 @@ def comp(box):
          else:
             print("error") 
             
-if ((start%2) == 0):
+if ((start%2) == 0):  #startiing the game
   player(box)
 else:
   comp(box)
